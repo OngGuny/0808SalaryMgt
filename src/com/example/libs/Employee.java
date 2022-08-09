3ed2b68002cfdc32aa80efe6245816d30e141b35
OngGuny <start1step@naver.com>
2022-08-09 10:23:45 +0900

    input test complete and little fix in Emps

---
+++ src/com/example/libs/Employee.java
@@ -0,0 +1,116 @@
+package com.example.libs;
+
+public class Employee {
+	private String empno,name,dpart;
+	private int base, night, fam,hopay,fampay,nightpay,total,salay;
+	
+	public Employee() {// 기본 생성자 
+	}
+
+	public Employee(String empno, String name, int base, int night, int fam) {
+		this.empno = empno;  //사원번호
+		this.name = name;	//이름
+		this.base = base;	//기본급
+		this.night = night;	//야근시간
+		this.fam = fam;		//가족수 
+	}
+
+	public String getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(String empno) {
+		this.empno = empno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDpart() {
+		return dpart;
+	}
+
+	public void setDpart(String dpart) {
+		this.dpart = dpart;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getNight() {
+		return night;
+	}
+
+	public void setNight(int night) {
+		this.night = night;
+	}
+
+	public int getFam() {
+		return fam;
+	}
+
+	public void setFam(int fam) {
+		this.fam = fam;
+	}
+
+	public int getHopay() {
+		return hopay;
+	}
+
+	public void setHopay(int hopay) {
+		this.hopay = hopay;
+	}
+
+	public int getFampay() {
+		return fampay;
+	}
+
+	public void setFampay(int fampay) {
+		this.fampay = fampay;
+	}
+
+	public int getNightpay() {
+		return nightpay;
+	}
+
+	public void setNightpay(int nightpay) {
+		this.nightpay = nightpay;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public int getSalay() {
+		return salay;
+	}
+
+	public void setSalay(int salay) {
+		this.salay = salay;
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"%-5s %5s %5s %5d %10d %7d %7d %10d %10d",
+				empno, name, dpart, base, hopay, fampay, nightpay, total, salay);
+	}// 변수 지정. 기본 생성자 만들고,  받는 변수 생성자 만들고 겟터셋터 셋팅하고  tostring 만듦 
+
+	
+	
+
+}
---
+++ src/com/example/libs/Input.java
@@ -0,0 +1,31 @@
+package com.example.libs; // com 안에 example 안에 libs 가 있다는것.  길어지니까 . 으로 구분함 
+import java.util.Scanner;
+
+public class Input {
+	private Employee[] emps;
+	private Scanner sc; 
+	int count=0;
+	
+	
+	public Input(Employee[] emps) {
+		this.emps = emps;
+		this.sc = new Scanner(System.in);
+		this.count=0;
+}
+	public int input() {
+		String y_n=null;
+		do {
+			System.out.print("사원번호 : "); String empno = this.sc.nextLine();
+			System.out.print("사원이름 : "); String name = this.sc.nextLine();
+			System.out.print("기본급 : "); int base = this.sc.nextInt();
+			System.out.print("야근시간 : ");int night = this.sc.nextInt();
+			System.out.print("가족수 : "); int fam = this.sc.nextInt();
+			this.sc.nextLine();//버퍼 날리기. 인트 다음에 스트링 나올때 버퍼 날리기. 다시 처음부터 다른사원 입력하니까. 
+			this.emps[count++]=new Employee(empno, name, base, night, fam);
+			System.out.println("Again(Y/N)? : ");
+			y_n = this.sc.next();
+		}while (y_n.equals("Y")||y_n.equals("y"));
+			return count;
+	}
+	
+}
